30ef8e4cce32e73595fd456f0ef9b4ab92dacd2d
0xmalloc <yinweixiong@baidu.com>
2014-09-16 15:17:18 +0800

    change cmake

--- collector/regex.example.cpp
+++ collector/regex.example.cpp
@@ -7,14 +7,23 @@ int main(int argc, char** argv)
     char* match_start = NULL;
     unsigned int match_len = 0;
 
-    const char* logstr = "NOTICE: xx  cost=98  xx";
-    const char* pattern = "cost=(\\d+)";
-
-    regex_ret_t ret = regex_search(pattern, logstr, &match_start, &match_len);
-    if(ret != REGEX_MATCHED_NO_VAL &&  ret != REGEX_MATCHED){
-        cout<< "Not Matched ret:" << ret <<endl;
-    }else{
-        cout<< "Matched ret:" << ret << endl; 
-        cout<< "Matched str:" << string(match_start, match_len) << endl; 
-    }
+    const char* logstr = "NOTICE: rpc_service  cost=98 ispv=1 method=add conn_time=11 read_time=5 write_time=19";
+    const char* pattern[] = {
+        "method=(\\w+)",
+        "cost=(\\d+)",
+        "rpc_service",
+        "error",
+        "rpc_time=(\\d+)",
+        "rpc_method=(\\w+)",
+    };
+    for(int i = 0; i < 6; i++){
+        cout<<"pattern:" << pattern[i] << endl;
+        regex_ret_t ret = regex_search(pattern[i], logstr, &match_start, &match_len);
+        if(ret != REGEX_MATCHED_NO_VAL &&  ret != REGEX_MATCHED){
+            cout<< "Not Matched ret:" << ret <<endl;
+        }else{
+            cout<< "Matched ret:" << ret << endl; 
+            cout<< "Matched str:" << string(match_start, match_len) << endl; 
+        }   
+    }   
 }
